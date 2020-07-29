2c2e20d6ca0d74ef59d5148c8dedd9ef451a436d
Bill Xiong <xiong.bill@yahoo.com>
2020-07-28 21:02:18 -0700

    added 3 new files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios'
-
-
-
-
+import MainData from "./MainData.js"
+import Information from "./Information.js"
+import Heading from "./Heading.js"
 
 function App() {
+  const [photo, setPhoto] = useState([])
+
+useEffect(() => {
+  axios
+  .get("https://api.nasa.gov/planetary/apod?api_key=bZq7dvX9tzei9EUf1T2t5ZY1vBwyXZGcgda4yg86date=2020-07-28")
+  .get("https://api.nasa.gov/planetary/apod?api_key=bZq7dvX9tzei9EUf1T2t5ZY1vBwyXZGcgda4yg86date=2020-07-27")
+  .then(response => {
+    console.log(photo);
+    console.log(response);
+    setDate(response.data.title)
+    setTitle(response.data.title)
+    setPhoto(response.data.url)
+    setInformation(response.data.information)
+  })
+  .catch(error => {
+    console.log("The data did not returned", error);
+  });
+}, []);
+
+
+
   return (
     <div className="App">
       <p>
