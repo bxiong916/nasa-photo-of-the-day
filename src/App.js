d6b948b3c88e1483a136dee5b88c90d5d324cac9
Bill Xiong <xiong.bill@yahoo.com>
2020-07-30 18:33:40 -0700

    done, ready for styling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from 'axios'
-import MainData from "./MainData.js"
-import Information from "./Information.js"
-import Heading from "./Heading.js"
+import axios from "axios";
+import Data from "./Data.jsx";
 
 function App() {
+  
   const [photo, setPhoto] = useState([])
+  const [date, setDate] = useState([])
+  const [explanation, setexplanation] = useState ([]);
+  const [title, setTitle] = useState ([]);
 
-useEffect(() => {
-  axios
-  .get("https://api.nasa.gov/planetary/apod?api_key=bZq7dvX9tzei9EUf1T2t5ZY1vBwyXZGcgda4yg86date=2020-07-28")
-  .get("https://api.nasa.gov/planetary/apod?api_key=bZq7dvX9tzei9EUf1T2t5ZY1vBwyXZGcgda4yg86date=2020-07-27")
-  .then(response => {
-    console.log(photo);
-    console.log(response);
-    setDate(response.data.title)
-    setTitle(response.data.title)
-    setPhoto(response.data.url)
-    setInformation(response.data.information)
-  })
-  .catch(error => {
-    console.log("The data did not returned", error);
-  });
-}, []);
+  useEffect(() => {
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=bZq7dvX9tzei9EUf1T2t5ZY1vBwyXZGcgda4yg86&date=2020-07-02")
+    .then( response => {
+      console.log(photo);
+      setDate(response.data.date)
+      setTitle(response.data.title)
+      setPhoto(response.data.url)
+      setexplanation(response.data.explanation)
+    })
+    .catch( error => {
+      console.log("Data not returned", error);
+    });
+   } , );
+   return (
+    <section className = "App">
+
+    <div className = "App-logo"></div>  
+    <div className = "App-header">NASA Photo of the Day
+    <div className = "App-link">The App Link</div>
+    <div className = "outerEyeOpen"> {title} </div>
+    <img src= {photo} alt= "NPOD"/>
+    <div className = "date"> {date}</div>
+
+    <div className = "NPOD"> 
+
+
+    </div>
 
 
 
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+
+
+    <article className= "explanation">{explanation}</article>
     </div>
   );
+    <div className = "outerEyeClose"></div>
+
+  </section>
+   );
 }
 
 export default App;
